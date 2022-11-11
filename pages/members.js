c80d33b652ad1e327444348f24cf599f25de0e01
vannguyendeptrai <vannguyen2426@gmail.com>
2022-11-11 01:37:07 +0100

    adding nextAuth, router, session, detecting subcriber
    adding nextAuth, router, session, detecting if user is subcriber

---
+++ pages/members.js
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+
+export default function Members(){
+    const router = useRouter()
+    const { data: session, status} = useSession()
+    const loading = status === 'loading'
+    if(loading){
+        return null
+    }
+    if(!session){
+        router.push('/')
+        return
+    }
+    if (!session.user.isSubscriber) {
+        router.push('/join')
+        return
+    }
+    return (
+        <>
+            <Head>
+                <title>Private Area</title>
+                <meta name="description" content="Private Area"/>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className="text-center">
+                <h1 className="mt-20 font-extrabold text-2xl">Private Area</h1>
+                <p className="mt-10">Thank you for being a member</p>
+                <p className="mt-10">You now have access to:</p>
+            </div>
+        </>
+    )
+}
